77180246404b77fcae320cc1d6c4245176f828ba
mojw2 <mojw2@LAPTOP-ILJU76A1.attlocal.net>
2022-08-01 19:19:43 -0400

    Initialize adding and multiplying two methods

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -7,4 +7,14 @@ public class DemoTest {
 		System.out.println("Test");
 	}
 
+	public void gitHub(double a, double b) {
+		
+		System.out.println(a + b);
+		
+	}
+	
+	public static void gitHubTwo(int a, int b) {
+		
+		System.out.println(a * b);
+	}
 }
